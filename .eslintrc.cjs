27097598d12d3e2410ef71c3b6afe53d13d749c8
nasustim <mitsuhibino@gmail.com>
2023-02-20 02:03:23 +0900

    add eslint settings
    reference: https://typescriptbook.jp/tutorials/eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.eslint.json",
+    tsconfigRootDir: __dirname,
+  },
+  ignorePatterns: [
+    "dist",
+    "storybook-static"
+  ],
+  extends: [
+    "airbnb-base",
+    "airbnb-typescript/base",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+  ],
+  rules: {
+    "import/prefer-default-export": "off",
+    "@typescript-eslint/quotes": ["error", "double"],
+  },
+}
